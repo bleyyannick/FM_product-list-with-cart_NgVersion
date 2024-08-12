c96e380eeb8e310f5c9db949e9abf25041acc3df
Yannick Bley <bleyyannickpro@gmail.com>
2024-08-12 13:39:00 +0200

    wip: add static assets and data.json in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+ import  data  from '../assets/data.json';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+    <main>
+      <img [src]="thumbnail" alt="thumbnail" />
+    </main>
+  `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'FM_product-list-with-cart';
+  data:any = data;
+  thumbnail = this.data[0].image.thumbnail;
+
+   constructor() {
+  }
 }
